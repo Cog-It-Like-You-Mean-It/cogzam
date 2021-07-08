42593b556f58019b2eefd782ef6c1605efeae227
Joseph Semrai <josephsemrai@gmail.com>
2021-07-08 17:05:07 -0400

    Database updates

--- cogzam/database.py
+++ cogzam/database.py
@@ -3,7 +3,7 @@
 
 class Database:
     def __init__():
-        #song_metadata = {}
+        # song_metadata = {}
         database = {}
 
     def store_fingerprint(self, fingerprints, song_title, times):
@@ -13,21 +13,23 @@ class Database:
             f = fingerprints[i]
 
             if f in self.database:
-                self.database[f].append((song_title, time[i]))
+                self.database[f].append((song_title, times[i]))
             else:
-                self.database[f] = [(song_title, time[i])]
-
-        return
+                self.database[f] = [(song_title, times[i])]
 
     def search_fingerprint(self, fingerprints):
         # key: fingerprints, value: (song, time_of_occurence)
         # all fingerprints of a song are given, return most likely song
 
-        import collections.Counter
-        retrieved_fingerprint = self.database[fingerprint]
+        from collections import Counter
+        c = Counter()
 
-        # consider returning counts here or in notebook
-        return retrieved_fingerprint
+        for f in fingerprints:
+            if f in self.database:
+                c[("song title", "time delay"))] += 1
+
+            # consider returning counts here or in notebook
+        return song
 
     '''def save_song(self, song_name, author):
         # save song metadata with a unique ID
@@ -37,12 +39,13 @@ class Database:
     def load_database(self):
         import pickle
 
-        with open("fingerprints.pkl", mode="rb") as fingerprints_file:
-            self.database = pickle.load(fingerprints_file)
+        with open("fingerprints.pkl", mode = "rb") as fingerprints_file:
+            self.database=pickle.load(fingerprints_file)
 
-        '''with open("song_metadata.pkl", mode="rb") as metadata_file:
-            self.song_metadata = pickle.load(metadata_file)'''
         return
 
-    def save_database():
+    def save_database(self):
+
+        with open("fingerprints.pkl", mode = "wb") as fingerprints_file:
+            pickle.dump(self.database, fingerprints_file)
         return
