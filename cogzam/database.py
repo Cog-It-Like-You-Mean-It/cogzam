fadbc7a69f7974cc7483c6b42ac92e3baca2c8cf
Joseph Semrai <josephsemrai@gmail.com>
2021-07-09 10:40:51 -0400

    Finish database
    Co-authored-by: hyojp13 <hyojp13@users.noreply.github.com>

--- cogzam/database.py
+++ cogzam/database.py
@@ -1,16 +1,17 @@
 # Example data structure
 import pickle
+from collections import Counter
 
 
 class Database:
     def __init__():
-        # song_metadata = {}
         database = {}
 
     def store_fingerprint(self, fingerprints, song_title, times):
         # key: (freq, neighbor_freq, time_delta)), value: (song, time_of_occurence)
         # store fingerprints of 1 song
-        for i in in range(len(fingerprints)):
+
+        for i in range(len(fingerprints)):
             f = fingerprints[i]
 
             if f in self.database:
@@ -19,27 +20,28 @@ class Database:
                 self.database[f] = [(song_title, times[i])]
 
     def search_fingerprint(self, fingerprints):
-        # key: fingerprints, value: (song, time_of_occurence)
+        # key: fingerprints, value: (song, time_of_occurance)
         # all fingerprints of a song are given, return most likely song
 
-        from collections import Counter
-        c = Counter()
-
+        # find the time_of_occurance for first fingerprint
+        l = []
         for f in fingerprints:
             if f in self.database:
-                c[("song title", "time delay"))] += 1
+                for match in self.database[f]:
+                    l.append(match[0])
 
             # consider returning counts here or in notebook
-        return song
+        c = Counter(l)
+        return c.most_common(1)[0][0]
 
     def load_database(self):
         import pickle
 
-        with open("fingerprints.pkl", mode = "rb") as fingerprints_file:
-            self.database=pickle.load(fingerprints_file)
+        with open("fingerprints.pkl", mode="rb") as fingerprints_file:
+            self.database = pickle.load(fingerprints_file)
 
         return
 
     def save_database(self):
-        with open("fingerprints.pkl", mode = "wb") as fingerprints_file:
-            pickle.dump(self.database, fingerprints_file, "wb"))
+        with open("fingerprints.pkl", mode="wb") as fingerprints_file:
+            pickle.dump(self.database, fingerprints_file)
